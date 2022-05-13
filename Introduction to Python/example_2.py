b2bc121d3c27f6a64e6423967268ffa91bfe9d62
Bakashev Sergey <BakashevSergey@gmail.com>
2022-05-13 18:05:03 +0300

    Выполнение второго тестового задания 1. Увеличение значения var_int в 3.5 раза 2. уменьшение значения var_float на -1 3. Разделение var_int на var_float, а затем big_int на var_float. 4. Измените значение переменной var_str на NoNoYesYesYes". При формировании нового значения используйте операции конкатенации (+) и повторения строки (*).

---
+++ Introduction to Python/example_2.py
@@ -0,0 +1,17 @@
+#Практическое задание к главе типы данных
+#Exercise 1
+var_int=10
+var_float=8.4
+var_str='No'
+#Увеличение значения var_int в 3.5 раза
+big_int=var_int *3.5
+print(big_int)
+# уменьшение значения var_float на -
+var_float-=1
+print(var_float)
+
+#Разделение var_int/var_float and big_int/var_float
+print(var_int/var_float)
+print(big_int/var_float)
+var_str = var_str + 'No' + ('Yes' *3)
+print(var_str)
