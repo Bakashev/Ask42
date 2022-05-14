a07d26c6a2da8ddbe16fdf09d55b419b8433da47
Bakashev Sergey <BakashevSergey@gmail.com>
2022-05-14 15:32:09 +0300

    Тренировка по вводу и выводу данных 1. Задаем возрост и имя использую input 2. преоброзуем введенные значения из строки в числовой формат

---
+++ Introduction to Python/data_input_output.py
@@ -0,0 +1,10 @@
+#Ввод ввод данных
+age=input("Your age: ") # При вводе числа всеравно возвращает строку
+name=input("Your name: ")
+print("My age {0} my name {1}".format(age,name))
+
+#Ввод числа и выполнение арефмитической операции
+x=int(input("Enter nuber X:"))
+y=float(input("Enter number Y: "))
+sum=x+y
+print("You need pay: {0}".format(sum))
