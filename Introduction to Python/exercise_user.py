54917141c4dd8a8ba5985fc31c7f9c41ddcc8911
Bakashev Sergey <BakashevSergey@gmail.com>
2022-05-14 15:43:14 +0300

    Ouotput on screen Name. Age, street use format

--- Introduction to Python/exercise_user.py
+++
@@ -1,5 +0,0 @@
-#Выполненеи тестового задания по вводу и выводу данных
-name = input("Enter your Name: ")
-age = float(input("Enter your Age: "))
-street = input("Enter your street: ")
-print(" This is {0}\n It is {1}\n He live in {2}".format(name,age,street))
