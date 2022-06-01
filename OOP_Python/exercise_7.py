f349fb5fba9a9d0709ede59e083db3bcf6d4844f
Bakashev Sergey <BakashevSergey@gmail.com>
2022-06-01 18:32:46 +0300

    Добавлени в класс Room атрибутов, удаление атрибута square, вместо атриббута добавился метод рассчитывающий площадь

--- OOP_Python/exercise_7.py
+++ OOP_Python/exercise_7.py
@@ -3,18 +3,25 @@ class WinDoor:
     def __init__(self,x,y):
         self.square=x*y
 
-
 # Созадние класса комнта
 
 class Room:
     def __init__(self,x,y,z):
-        self.square=2*z*(x+y)
+        #self.square=2*z*(x+y)
+        self.height = z
+        self.width = y
+        self.length = x
         self.wd=[]
+    # Метод для расчета площади
+    def setSquare (self):
+        self.square=2*self.height*(self.width+self.length)
+        return self.square
+    # Метод для добавления нового окна или двери
     def addWD(self,w,h):
         self.wd.append(WinDoor(w,h))
-
+    # Метод для расчет рабочей поверхности
     def workSurface(self):
-        new_square=self.square
+        new_square=self.setSquare()
         for i in self.wd:
             new_square-=i.square
         return new_square
@@ -22,7 +29,7 @@ class Room:
 
 # основной код
 squreRoom=Room(4,5,6)
-print(squreRoom.square)
+#print(squreRoom.square)
 squreRoom.addWD(2,3)
 squreRoom.addWD(2,3)
 squreRoom.addWD(4,2)
